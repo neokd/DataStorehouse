352e71a7f3ac8508471fd3de92c397b9595de435
neokd <neokd@protonmail.com>
2023-05-29 18:57:50 +0530

    Update gh pages

--- website/vite.config.js
+++ website/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://neokd.github.io/DataBucket/',
+
   plugins: [react()],
 })
