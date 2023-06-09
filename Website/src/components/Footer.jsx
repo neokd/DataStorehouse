ff23d5d354ea5585de276834569b00c3fbfa0e96
neokd <neokd@protonmail.com>
2023-06-09 19:45:36 +0530

    Hindi Corpus Added

--- Website/src/components/Footer.jsx
+++ Website/src/components/Footer.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom"
 
 
 function Footer() {
   return (
     <div className="bottom-0 text-center">
       <div className='dark:bg-customGray/5 dark:text-white/50 
-               py-8 flex flex-col justify-center items-center'>
-        &copy; DataBucket 2023 | Developed by Coders for Coders 👩‍💻🧑‍💻
+               py-8 flex flex-rows justify-center items-center'>
+        &copy;<Link to='https://github.com/neokd/DataBucket'>DataBucket</Link>  2023 | Developed by Coders for Coders 👩‍💻🧑‍💻
 
       </div>
     </div>
