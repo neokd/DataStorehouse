dc1db50d2ba683d8ba9b7faf1d961a5232fa4228
Kuldeep <71772185+neokd@users.noreply.github.com>
2023-06-14 21:35:02 +0530

    Password dataset added

---
+++ script/password.py
@@ -0,0 +1,18 @@
+import string
+import random
+import time
+
+def generate_password(length):
+    password = ''
+    for _ in range(length):
+        password += random.choice(string.ascii_letters + string.digits)
+    return password
+
+if __name__ == '__main__':
+    start = time.time()
+    with open('keyboard_combination.txt','w') as f:
+        for i in range(10000000):
+            f.write(generate_password(10)+'\n')
+    end = time.time()
+    print('Took {} seconds to generate 1 million passwords'.format(end-start))
+    
