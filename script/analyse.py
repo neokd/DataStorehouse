4ed3e1198920716ac226165b40bdf19a8bf75ad8
rtiop <101009997+rtiop@users.noreply.github.com>
2023-07-04 10:25:43 +0000

    Updating script

--- script/analyse.py
+++ script/analyse.py
@@ -1,10 +1,21 @@
 import csv
 import sys
 
+# Read the file into the script
 if len(sys.argv) != 2:
-  print("Usage: python analyse.py file_name")
-  return 1
+    raise ValueError("Usage: python analyse.py file_name")
 file_name = sys.argv[1]
-with open(file_name, 'r') as file:
-  reader = csv.DictReader(file)
-  print(type(reader))
+
+# Read a CSV file
+if file_name[-4:] == ".csv":
+    with open(file_name, 'r') as file:
+        reader = csv.DictReader(file)
+        print(type(reader))
+
+# Read a JSON file
+elif file_name[-5:] == ".json":
+    raise NotImplementedError
+
+
+else:
+    raise ValueError("Only CSV and JSON file format are currently supported")
