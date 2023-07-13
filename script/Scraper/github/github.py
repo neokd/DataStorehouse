97b892e6d51e139f17722c233a24e8f7fa4320a2
Meet Rajput <rajputmeet10@gmail.com>
2023-07-13 11:17:39 +0530

    get_socials method added in github.py

--- script/Scraper/github/github.py
+++ script/Scraper/github/github.py
@@ -59,9 +59,26 @@ class Github:
             print(ex)
             return "Error: Failed to fetch readme."
 
+    def get_socials(self) -> str:
+        user_profile=self.scrape_profile()
+        try:
+            social_li_elements = user_profile.select('ul.vcard-details li')
+            social_media_urls = []
+
+            for li_element in social_li_elements:
+                link_element = li_element.find('a')
+                if link_element is not None:
+                    social_media_urls.append(link_element['href'])
+
+            urls_string = '\n'.join(social_media_urls)
+
+            return urls_string
+        except Exception as ex:
+            print(ex)
+            return "Error: Failed to fetch socials."
 
 
         
 if __name__ == '__main__':
     github = Github('neokd')
-    print(github.user_readme())
+    print(github.get_socials())
