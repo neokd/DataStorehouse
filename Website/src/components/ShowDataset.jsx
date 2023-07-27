5631ff28be1bcc351611e4b616d1d1c57a1e0b49
chasserb <chasserb@childandfamilyagency.org>
2023-07-26 20:12:09 -0400

    Fix card layout

--- Website/src/components/ShowDataset.jsx
+++ Website/src/components/ShowDataset.jsx
@@ -222,22 +222,22 @@ function ShowDataset() {
                         <p className='text-lg line-clamp-2'>{item.description}</p>
                         <p className='text-md'>File Type: {Array.isArray(item.fileType) ? item.fileType.join(', ') : item.fileType}</p>
                         <p className='text-md'>Size: {item.size}</p>
-                        <span className='flex justify-end lg:justify-between'>
-                          <span className='justify-start hidden lg:block'>
+                        <span className='flex flex-wrap justify-end lg:justify-between'>
+                        <div className='flex flex-wrap justify-start lg:justify'>
                             {item.tag.map((tag) => (
                               <span
                                 key={tag}
-                                className='text-md font-bold bg-amber-500 rounded-full text-gray-900 px-2 py-1 lg:mr-2'
+                                className='text-md font-bold bg-amber-500 rounded-full text-gray-900 py-1 mr-5 mb-5 lg:mr-2 lg:mb-2 px-2 lg:px-4 truncate'
                               >
                                 {tag}
                               </span>
                             ))}
-                          </span>
+                          </div>
                           {
                             item.githubPath.match(/github.com/g) ? (
                               <button onClick={getButtonAnalytics}>
                                 <Link to={item.githubPath} className=' ' target='_blank'>
-                                  <div className='p-3 border-amber-500 rounded-full bg-amber-500 '>
+                                  <div className='p-3 border-amber-500 rounded-full bg-amber-500 flex items-center '>
                                     <svg
                                       xmlns='http://www.w3.org/2000/svg'
                                       viewBox='0 0 24 24'
@@ -246,7 +246,7 @@ function ShowDataset() {
                                       strokeWidth='2'
                                       strokeLinecap='round'
                                       strokeLinejoin='round'
-                                      className='w-6 h-6 '
+                                      className='w-6 h-6 lg:w-8 lg:h-8'
                                     >
                                       <path d='M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4'></path>
                                       <path d='M9 18c-4.51 2-5-2-7-2'></path>
