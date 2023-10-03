3d05320698f9a0ee9e2148c4ad28a9e895ee518c
Kuldeep <71772185+neokd@users.noreply.github.com>
2023-10-03 10:20:37 +0530

    moved scrapper

---
+++ scraper/reddit/reddit.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+
+class Reddit:
+    def __init__(self) -> None:
+        pass
---
+++ scraper/ycombinator/ycombinator.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+class YCombinator:
+    def __init__(self,url) -> None:
+        self.url = url
+
+
+if __name__ == '__main__':
+    YCombinator('https://news.ycombinator.com/')
